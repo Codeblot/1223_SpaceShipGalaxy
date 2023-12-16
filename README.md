6181cd41de95b6915406a93d2f11c3fbbf42d210
Mike Hamby <artistmikejay@gmail.com>
2023-12-16 16:14:13 -0600

    Added new notes to README.MD that specify changes

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-#An Introduction to Game Development in Python and Godot - with Christian Koch
+# An Introduction to Game Development in Python and Godot - with Christian Koch
 
 I am learning how Python and Godot can be utilized together for making games. This is my first round at coding something substantial.
+
+## VLOG
+
+### June 2023
+1. Intermittent dev times led me to stop at 20.Meteor Dodger 3 - Spaceship Movement. Had troubles with curser still being visible. Need to stop as main job is picking up in amount of work
+
+### December 2023
+1. Started coding again. Worked on adding laser spawing from the ship on click. This was taught in 22.Meteor Dodge 6 - Creating a laser. Still had issues with cursor displaying an not being hidden.
+
+2. Reorganized the groups at the end of the file to draw the groups on the correct layers. Since all is drawn on top in order of appearence, this made the laser more beleivable that it was coming from a gun mounted underneath the spacecraft.
+
+3. 4:03pm - Found the method on Python-CE Docs. https://pyga.me/docs/ref/mouse.html#pygame.mouse.set_visible. 
+
+
+```python
+pygame.mpuse.set_visible()```
+
+Since I already defined the class SpaceShip, I placed the new code in the def update as 
+
+```python
+pygame.mouse.set_visible(False)```
+
+This created the desired change.
+
+5. Added killswitch for laser after leaving the active window to midigate frame data.
+
+```python
+if self.rect.centery <-50:
+self.kill()
+```
--- SpaceShipGalaxy.py
+++ SpaceShipGalaxy.py
@@ -1,5 +1,7 @@
 import pygame, sys, random
 
+#Describe classes to be used and their base properties
+#Spaceship that is controlled by mouse movement (pos)
 class SpaceShip(pygame.sprite.Sprite):
         def __init__(self,path,x_pos,y_pos):
                 super().__init__()
@@ -8,7 +10,9 @@ class SpaceShip(pygame.sprite.Sprite):
 
         def update(self):
                 self.rect.center = pygame.mouse.get_pos()
+                pygame.mouse.set_visible(False)
                 self.screen_constrain()
+
         
         def screen_constrain(self):
                 if self.rect.right >= 1280:
@@ -20,6 +24,7 @@ class SpaceShip(pygame.sprite.Sprite):
                 if self.rect.bottom >= 720:
                         self.rect.bottom = 720
 
+#Meteor Shower that spawns on load
 class Meteor(pygame.sprite.Sprite):
         def __init__(self,path,x_pos,y_pos,x_speed,y_speed):
                 super().__init__()
@@ -35,16 +40,19 @@ class Meteor(pygame.sprite.Sprite):
                 if self.rect.centery >= 800:
                         self.kill()
 
+#Laser for to be drawn on mouse button down
 class Laser(pygame.sprite.Sprite):
-        def __init__(self,path,x_pos,y_pos,speed):
+        def __init__(self,path,pos,speed):
                 super().__init__()
                 self.image = pygame.image.load('assets/Laser.png')
                 self.rect = self.image.get_rect(center = pos)
                 self.speed = speed
 
         def update(self):
-                self.rect.cery -= self.speed
+                self.rect.centery -= self.speed
                 
+                if self.rect.centery <-50:
+                        self.kill()
 
 pygame.init() # Initiate pygame
 screen = pygame.display.set_mode((1280,720)) # Create display surface
@@ -59,6 +67,8 @@ spaceship_group.add(spaceship)
 meteor_group = pygame.sprite.Group() #Sprite Meteor Group
 METEOR_EVENT = pygame.USEREVENT
 pygame.time.set_timer(METEOR_EVENT,100)
+
+laser_group = pygame.sprite.Group()
 
 while True: # Game loop
         for event in pygame.event.get(): # Check for events / Player input
@@ -77,11 +87,20 @@ while True: # Game loop
                         random_y_speed = random.randrange(4,10)
                         meteor = Meteor(meteor_path,random_x_pos,random_y_pos,random_x_speed,random_y_speed)
                         meteor_group.add(meteor)
+                        
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                        new_laser = Laser('assets/Laser.png', event.pos, 10)
+                        laser_group.add(new_laser)
 
         screen.fill((42,45,51))
+        
+        laser_group.draw(screen)
         spaceship_group.draw(screen)
-        spaceship_group.update()
         meteor_group.draw(screen)
+
+        laser_group.update()
+        spaceship_group.update()
         meteor_group.update()
+
         pygame.display.update() # Draw frame
         clock.tick(120) # Control framerate
